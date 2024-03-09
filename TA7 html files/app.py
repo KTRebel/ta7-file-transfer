20bd0b0e04ac3c0c6981e3eb1cf8b879619b04b3
KTRebel <156741366+KTRebel@users.noreply.github.com>
2024-03-08 22:07:49 -0800

    HTML for home, log in, create account
    Go Back button doesn't work, but it shouldn't be needed. Feel free to add and change classes as you need to so your back-end programs work with these front-end files.

---
+++ TA7 html files/app.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+import uvicorn
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+def get_html() -> HTMLResponse:
+    with open("home.html") as html:
+        return HTMLResponse(content=html.read())
+    
+@app.get("/create", response_class=HTMLResponse)
+def get_html() -> HTMLResponse:
+    with open("create.html") as html:
+        return HTMLResponse(content=html.read())
+    
+@app.get("/signin", response_class=HTMLResponse)
+def get_html() -> HTMLResponse:
+    with open("signin.html") as html:
+        return HTMLResponse(content=html.read())
+    
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=6543)
